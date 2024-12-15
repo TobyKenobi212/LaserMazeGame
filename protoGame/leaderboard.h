456894c2d35aaa4ece9e4b29f56a0cb50ee96db3
phrygianmind <thaibenjamin64@gmail.com>
2024-12-15 12:52:51 -0800

    Update leaderboard.h description

--- protoGame/leaderboard.h
+++ protoGame/leaderboard.h
@@ -1,5 +1,8 @@
 /*
     File containing class methods for Leaderboard class
+    Author(s): Toby Pham, Tri Nguyen, Benjamin Thai
+    Last updated 12/15/2024
+    Version 1.03
 */
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
